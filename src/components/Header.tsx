6cfa91dd3faf2e638ec6ca557d8183635fb31011
Bedant Chhetri <bedantchhetri7@gmail.com>
2025-06-06 00:06:01 +0530

    feat: update Header component styling and layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import Button from "./Button"
 const Header = () => {
   return (
     <div className="">
-        <div className="bg-gray-600 max-w-7xl mx-auto p-4 rounded-b-2xl flex items-center justify-between">
-            <div className="text-white font-bold text-2xl">Brainly</div>
+        <div className="bg-gray-600 max-w-6xl mx-auto px-4 py-2 rounded-b-2xl flex items-center justify-between">
+            <div className="text-white font-bold text-2xl cursor-pointer">Brainly</div>
             <div className="flex gap-4">
                 <Button text={"Share"}/>
                 <Button text={"Add Memory"}/>
