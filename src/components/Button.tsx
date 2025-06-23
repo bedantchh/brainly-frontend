4101792d6a9e57d548345bf622546ff55e4eef38
Bedant Chhetri <bedantchhetri7@gmail.com>
2025-06-23 23:08:38 +0530

    feat: enhance Button and Header components with SVG icons and improved layout

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,12 @@
 type ButtonProps = {
   text: string;
-  onClick?: any
+  onClick?: ()=> void;
+  svg?:React.ReactNode
 };
 
-const Button = ({ text,onClick }: ButtonProps) => {
+const Button = ({ text,onClick,svg }: ButtonProps) => {
   return (
-    <button onClick={onClick} className="bg-gray-400 text-white px-4 py-2 rounded-xl cursor-pointer hover:px-5 hover:rounded-lg hover:bg-gray-500 transform transition-all">{text}</button>
+    <button onClick={onClick} className="flex justify-between gap-2 bg-gray-400 text-white px-4 py-2 rounded-xl cursor-pointer hover:scale-105 hover:rounded-lg hover:bg-gray-500 transform transition-all">{svg}{text}</button>
   )
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,8 +6,26 @@ const Card = () => {
         <h1 className="font-bold text-base">
           <span>❤️</span> Title goes here
         </h1>
-        <div className="mt-4 flex justify-between text-sm font-mediumtext-slate-900">
-          <p>Delete</p>
+        <div className="mt-4 flex justify-between items-center text-sm font-mediumtext-slate-900">
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={1}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-trash hover:cursor-pointer"
+            >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 7l16 0" />
+            <path d="M10 11l0 6" />
+            <path d="M14 11l0 6" />
+            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+            </svg>
           <p>26/06/2025</p>
         </div>
       </div>
