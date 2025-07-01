9d8ff09ade64b0f9331319ccf936194ced4df014
Bedant Chhetri <bedantchhetri7@gmail.com>
2025-07-02 00:39:06 +0530

    fix: correct username reference and update SignIn function to handle JWT token

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -2,25 +2,35 @@ import { useRef } from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import axios from "axios";
+import { useNavigate } from "react-router";
 
 
 const SignIn = () => {
-  const BACKEND_URL= import.meta.env.BACKEND_URL
-  const userNameRef = useRef<HTMLInputElement>(null)
+  const BACKEND_URL= import.meta.env.VITE_BACKEND_URL
+  const usernameRef = useRef<HTMLInputElement>(null)
   const passwordRef = useRef<HTMLInputElement>(null)
+  const navigate = useNavigate()
 
-  function submit(){
-    const username = userNameRef.current?.value
+  async function submit(){
+    try{
+    const username = usernameRef.current?.value
     const password = passwordRef.current?.value
-    console.log(username,password)
-    axios.post(`${BACKEND_URL}/api/v1/signup`,{username,password})
+    const response = await axios.post<{token:string}>(`${BACKEND_URL}/api/v1/signin`,{username,password})
+    const jwt = response.data?.token
+    if(jwt){
+      localStorage.setItem("token",jwt)
+      navigate("/dashboard")
+    }
+    }catch(e){
+      console.error(e);
+    }
   }
 
   return <div className="flex justify-center items-center h-full">
     <div className="rounded-2xl bg-gradient-to-b from-shadow-green-700 via-teal-900 to-shadow-green-700 shadow-2xl p-4 flex flex-col items-center gap-6  min-w-xs md:min-w-sm">
         <h1 className="text-white font-bold text-2xl ">Sign in</h1>
         <div className="flex flex-col gap-4 w-full">
-            <Input refrence={userNameRef} label="Username" type="email"/>
+            <Input refrence={usernameRef} label="Username" type="email"/>
             <Input refrence={passwordRef} label="Password" type="password"/>
         </div>
         <Button onClick={submit} text="Sign In"/>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,30 +2,36 @@ import { useRef } from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import axios from "axios";
-
+// import { useNavigate } from "react-router";
 
 const SignUp = () => {
-  const BACKEND_URL= import.meta.env.VITE_BACKEND_URL
-  const userNameRef = useRef<HTMLInputElement>(null)
-  const passwordRef = useRef<HTMLInputElement>(null)
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  // const navigate = useNavigate();
 
-  async function submit(){
-    const username = userNameRef.current?.value
-    const password = passwordRef.current?.value
-    console.log(username,password)
-    await axios.post(`${BACKEND_URL}/api/v1/signup`,{username,password})
+  async function submit() {
+    try {
+      const username = usernameRef.current?.value;
+      const password = passwordRef.current?.value;
+      await axios.post(`${BACKEND_URL}/api/v1/signup`, { username, password });
+    } catch (e) {
+      console.error(e);
+    }
   }
 
-  return <div className="flex justify-center items-center h-full">
-    <div className="rounded-2xl bg-gradient-to-b from-shadow-green-700 via-teal-900 to-shadow-green-700 shadow-2xl p-4 flex flex-col items-center gap-6  min-w-xs md:min-w-sm">
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="rounded-2xl bg-gradient-to-b from-shadow-green-700 via-teal-900 to-shadow-green-700 shadow-2xl p-4 flex flex-col items-center gap-6  min-w-xs md:min-w-sm">
         <h1 className="text-white font-bold text-2xl ">Sign Up</h1>
         <div className="flex flex-col gap-4 w-full">
-            <Input refrence={userNameRef} label="Username" type="email"/>
-            <Input refrence={passwordRef} label="Password" type="password"/>
+          <Input refrence={usernameRef} label="Username" type="email" />
+          <Input refrence={passwordRef} label="Password" type="password" />
         </div>
-        <Button onClick={submit} text="Sign Up"/>
+        <Button onClick={submit} text="Sign Up" />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default SignUp;
