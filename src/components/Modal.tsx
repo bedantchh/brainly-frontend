c948a57fcc43e3bd9aaee420c037372a993e509b
Bedant Chhetri <bedantchhetri7@gmail.com>
2025-07-03 14:44:35 +0530

    feat: enhance Modal component with ToggleButtons and submit functionality

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,44 +1,95 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import Input from "./Input";
+import ToggleButtons from "./ToggleButtons";
+import axios from "axios";
 
-interface ModalProps{
+interface ModalProps {
   open: boolean;
-  onClose: ()=> void;
+  onClose: () => void;
+  onSubmitSuccess: ()=> void
 }
 
-const Modal = ({open,onClose}:ModalProps) => {
+const Modal = ({ open, onClose,onSubmitSuccess }: ModalProps) => {
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
   const modalRef = useRef<HTMLDivElement>(null);
-  useEffect(()=>{
-    const handleClickOutside = (event: MouseEvent)=>{
-      if(modalRef.current && !modalRef.current.contains(event.target as Node)){
+  const titleRef = useRef<HTMLInputElement>(null);
+  const linkRef = useRef<HTMLInputElement>(null);
+  const [isSelected, setSelected] = useState("youtube");
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
         onClose();
       }
+    };
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside);
     }
-    if(open){
-        document.addEventListener("mousedown",handleClickOutside);
-      }
-      
-      return ()=> {
-        document.removeEventListener("mousedown",handleClickOutside);
-      }
-  },[open,onClose])
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [open, onClose]);
+  async function handleSubmit() {
+    const title = titleRef.current?.value;
+    const link = linkRef.current?.value;
+    const type = isSelected;
+    try {
+      await axios.post(
+        `${BACKEND_URL}/api/v1/content`,
+        {
+          title: title,
+          link: link,
+          type: type,
+        },
+        { headers: { authorization: localStorage.getItem("token") } }
+      );
+      onSubmitSuccess()
+      onClose()
+    } catch (e) {
+      console.error(e);
+    }
+  }
   return (
     <div>
-      {open ? <div className="absolute top-0 left-0 z-50 w-full h-full flex items-center justify-center backdrop-blur-xs">
-      <div ref={modalRef} className="p-4 rounded-2xl bg-gradient-to-b from-shadow-green-700 via-teal-900 to-shadow-green-700 min-w-xs md:min-w-md text-shadow-green-50">
-        <div className="flex justify-between">
-          <h1 className="mb-4 font-bold text-2xl ">Add New Content</h1>
-          <p className="cursor-pointer" onClick={onClose}>X</p>
+      {open ? (
+        <div className="absolute top-0 left-0 z-50 w-full h-full flex items-center justify-center backdrop-blur-xs">
+          <div
+            ref={modalRef}
+            className="p-4 rounded-2xl bg-gradient-to-b from-shadow-green-700 via-teal-900 to-shadow-green-700 min-w-xs md:min-w-md text-shadow-green-50"
+          >
+            <div className="flex justify-between">
+              <h1 className="mb-4 font-bold text-2xl ">Add New Content</h1>
+              <p className="cursor-pointer" onClick={onClose}>
+                X
+              </p>
+            </div>
+            <div className="flex flex-col gap-4">
+              <Input reference={titleRef} label="Title" type="text" />
+              <Input reference={linkRef} label="Link" type="text" />
+              <div className="flex gap-4 items-center">
+                <p>Content Type:</p>
+                <div className="flex gap-1 rounded-xl bg-shadow-green-950 p-1.5">
+                  <ToggleButtons
+                    onClick={() => setSelected("youtube")}
+                    active={isSelected === "youtube"}
+                    text="Youtube"
+                  />
+                  <ToggleButtons
+                    onClick={() => setSelected("twitter")}
+                    active={isSelected === "twitter"}
+                    text="Twitter"
+                  />
+                </div>
+              </div>
+              <Button onClick={handleSubmit} text="Add Memory" />
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4">
-        <Input label="Title" type="text" />
-        <Input label="Link" type="text" />
-        <Button text="Add Memory" />
-        </div>
-      </div>
-    </div> : null}
-
+      ) : null}
     </div>
   );
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,26 +1,80 @@
-import { useState } from "react";
-import Card from "../components/Card"
-import Header from "../components/Header"
-import Modal from "../components/Modal"
-
+import { use, useEffect, useState } from "react";
+import Card from "../components/Card";
+import Header from "../components/Header";
+import Modal from "../components/Modal";
+import axios from "axios";
 
 const Dashboard = () => {
-      const [open, setOpen] = useState(false);
+  // enum ContentType {
+  //   youtube = "youtube",
+  //   twitter = "twitter",
+  // }
+  interface ContentItem {
+    _id: string;
+    title: string;
+    link: string;
+    type: "twitter" | "youtube";
+  }
+  interface ApiResponse {
+    content: ContentItem[];
+  }
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+  const [open, setOpen] = useState(false);
+  const [userContent, setUserContent] = useState<ContentItem[]>([]);
+
+  useEffect(() => {
+    fetchContent();
+  }, []);
+
+  async function fetchContent() {
+    try {
+      const content = await axios.get<ApiResponse>(
+        `${BACKEND_URL}/api/v1/content`,
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
+      setUserContent(content.data?.content);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  console.log(userContent);
   return (
     <>
-        <Header onOpen={() => setOpen(true)} />
-        <Modal open={open} onClose={() => setOpen(false)} />
-        <div className="max-w-6xl mx-auto mt-12">
-          <div className="columns-1 md:columns-2 lg:columns-3 mx-auto gap-4 px-4 md:px-10">
-            
-            <Card title="Tit le" type="youtube" link="https://www.youtube.com/watch?v=WevY8WuTgJY"/>
-            <Card title="Title goes here" type="twitter" link="https://twitter.com/username/status/1939743280599900414"/>
-            <Card title="Tit le" type="youtube" link="https://www.youtube.com/watch?v=WevY8WuTgJY"/> 
-            <Card title="Tit le" type="youtube" link="https://www.youtube.com/watch?v=WevY8WuTgJY"/> 
-          </div>
+      <Header onOpen={() => setOpen(true)} />
+      <Modal onSubmitSuccess={fetchContent} open={open} onClose={() => setOpen(false)} />
+      <div className="max-w-6xl mx-auto mt-12">
+        <div className="columns-1 md:columns-2 lg:columns-3 mx-auto gap-4 px-4 md:px-10">
+          {userContent.map((c: ContentItem) => (
+            <Card key={c._id} link={c.link} title={c.title} type={c.type} />
+          ))}
+          {/* <Card
+            title="Tit le"
+            type="youtube"
+            link="https://www.youtube.com/watch?v=WevY8WuTgJY"
+          />
+          <Card
+            title="Title goes here"
+            type="twitter"
+            link="https://twitter.com/username/status/1939743280599900414"
+          />
+          <Card
+            title="Tit le"
+            type="youtube"
+            link="https://www.youtube.com/watch?v=WevY8WuTgJY"
+          />
+          <Card
+            title="Tit le"
+            type="youtube"
+            link="https://www.youtube.com/watch?v=WevY8WuTgJY"
+          /> */}
         </div>
-      </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
